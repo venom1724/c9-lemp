ebb4bb1b2048365e12e048e479888c47c42cc766
gesh <venom1724@gmail.com>
2017-07-28 04:21:43 +0300

    php 7.1

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ fi
 # Update Composer
 sudo /usr/bin/composer self-update
 
-# Add PHP7.0 Repository
+# Add PHP7.1 Repository
 LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-add-repository ppa:rwky/redis -y
 sudo apt-get update
@@ -22,10 +22,10 @@ sudo apt-get update
 # Update Heroku Toolbelt
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-# Install PHP7.0 & Redis
-sudo apt-get install -qq php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-phpdbg \
-php7.0-mbstring php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-tidy php7.0-dev \
-php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml redis-server
+# Install PHP7.1 & Redis
+sudo apt-get install -qq php7.1-fpm php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-mysql php7.1-phpdbg \
+php7.1-mbstring php7.1-gd php7.1-imap php7.1-ldap php7.1-pgsql php7.1-pspell php7.1-recode php7.1-tidy \
+php7.1-intl php7.1-gd php7.1-curl php7.1-zip php7.1-xml redis-server
 sudo apt-get purge -qq apache2 mysql-server mysql-client
 
 # Stop all the services
@@ -42,15 +42,15 @@ sudo service nginx stop
 # Listen port 80, change document root, setup indexes, configure PHP sock
 # set up the try_url thing (Drupal is not Worpress)...
 # Thankfully, I already modified this in the repo!
-sudo wget https://raw.githubusercontent.com/GabrielGil/c9-lemp/master/c9 --output-document=/etc/nginx/sites-available/c9
+sudo wget https://raw.githubusercontent.com/venom1724/c9-lemp/master/c9 --output-document=/etc/nginx/sites-available/c9
 sudo chmod 755 /etc/nginx/sites-available/c9
 sudo ln -s /etc/nginx/sites-available/c9 /etc/nginx/sites-enabled/c9
 
 
 # PHP:
-sudo sed -i 's/user = www-data/user = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
-sudo sed -i 's/group = www-data/group = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
-sudo sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php/7.0/fpm/pool.d/www.conf # Reduce number of processes..
+sudo sed -i 's/user = www-data/user = ubuntu/g' /etc/php/7.1/fpm/pool.d/www.conf
+sudo sed -i 's/group = www-data/group = ubuntu/g' /etc/php/7.1/fpm/pool.d/www.conf
+sudo sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php/7.1/fpm/pool.d/www.conf # Reduce number of processes..
 
 # Install Redis Driver
 cd ~
@@ -62,22 +62,22 @@ phpize
 make && sudo make install
 cd ..
 rm -rf phpredis
-sudo sh -c 'echo "extension=redis.so" > /etc/php/7.0/mods-available/redis.ini'
-sudo ln -sf /etc/php/7.0/mods-available/redis.ini /etc/php/7.0/fpm/conf.d/20-redis.ini
-sudo ln -sf /etc/php/7.0/mods-available/redis.ini /etc/php/7.0/cli/conf.d/20-redis.ini
+sudo sh -c 'echo "extension=redis.so" > /etc/php/7.1/mods-available/redis.ini'
+sudo ln -sf /etc/php/7.1/mods-available/redis.ini /etc/php/7.1/fpm/conf.d/20-redis.ini
+sudo ln -sf /etc/php/7.1/mods-available/redis.ini /etc/php/7.1/cli/conf.d/20-redis.ini
 
 # Install helper
-sudo wget https://raw.githubusercontent.com/GabrielGil/c9-lemp/master/lemp --output-document=/usr/bin/lemp
+sudo wget https://raw.githubusercontent.com/venom1724/c9-lemp/master/lemp --output-document=/usr/bin/lemp
 sudo chmod 755 /usr/bin/lemp
 
 # Start the party!
 sudo service nginx start
 sudo service nginx reload
-sudo service php7.0-fpm start
+sudo service php7.1-fpm start
 
 # Are we ready?
 echo Check all services are up.
 sleep 5
 sudo service nginx status
-sudo service php7.0-fpm status
+sudo service php7.1-fpm status
 lemp restart
